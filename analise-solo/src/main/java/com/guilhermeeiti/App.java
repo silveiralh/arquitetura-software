7197d0b885d0bf9cf262cb0712d51ba32fec7ef4
guilhermemiua <guilhermemiua@gmail.com>
2021-03-02 23:24:46 -0300

    Adicionado classe de Planilha com os atributos e getters/setters

--- analise-solo/src/main/java/com/guilhermeeiti/App.java
+++ analise-solo/src/main/java/com/guilhermeeiti/App.java
@@ -1,13 +1,9 @@
 package com.guilhermeeiti;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args )
+	{
+		System.out.println( "Hello World!" );
+	}
 }
---
+++ analise-solo/src/main/java/com/guilhermeeiti/Planilha.java
@@ -0,0 +1,76 @@
+package com.guilhermeeiti;
+
+public class Planilha {
+    private int texturaSolo;
+    private double fosforo;
+    private double potassio;
+    private double calcio;
+    private double magnesio;
+    private double enxofre;
+    private double aluminio;
+    private double acidezPotencial;
+    
+    public void setTexturaSolo(int texturaSolo) {
+        this.texturaSolo = texturaSolo;
+    }
+    
+    public double getTexturaSolo() {
+        return this.texturaSolo;
+    }
+    
+    public void setFosforo(double fosforo) {
+        this.fosforo = fosforo;
+    }
+    
+    public double getFosforo() {
+        return this.fosforo;
+    }
+    
+    public void setPotassio(double potassio) {
+        this.potassio = potassio;
+    }
+    
+    public double getPotassio() {
+        return this.potassio;
+    }
+        
+    public void setCalcio(double calcio) {
+        this.calcio = calcio;
+    }
+    
+    public double getCalcio() {
+        return this.calcio;
+    }
+    
+    public void setMagnesio(double magnesio) {
+        this.magnesio = magnesio;
+    }
+    
+    public double getMagnesio() {
+        return this.magnesio;
+    }
+    
+    public void setEnxofre(double enxofre) {
+        this.enxofre = enxofre;
+    }
+    
+    public double getEnxofre() {
+        return this.enxofre;
+    }
+    
+    public void setAluminio(double aluminio) {
+        this.aluminio = aluminio;
+    }
+    
+    public double getAluminio() {
+        return this.aluminio;
+    }
+        
+    public void setAcidezPotencial(double acidezPotencial) {
+        this.acidezPotencial = acidezPotencial;
+    }
+    
+    public double getAcidezPotencial() {
+        return this.acidezPotencial;
+    }
+}
