d94b3d29706dec4d8b43d986af48571209b9e1bd
Luiz Henrique Carvalho Silveira <silveira.lh@gmail.com>
2021-05-06 17:35:58 -0300

    Update na Classe Atividade4Test para teste de Teores do Solo
    Assertions dos valores de fosforo, potassio, calcio, magnesio, enxofre e aluminio ideiais junto com os campos Scmol, CTCmol, VAtual, TeorMO e Carbono

--- analise-solo/src/test/java/com/guilhermeeiti/Atividade4Test.java
+++ analise-solo/src/test/java/com/guilhermeeiti/Atividade4Test.java
@@ -1,5 +1,31 @@
 package com.guilhermeeiti;
 
+import static org.junit.Assert.*;
+
+import com.guilhermeeiti.classes.Solo;
+import com.guilhermeeiti.enums.TexturaSolo;
+
 public class Atividade4Test {
-	//teste de commit no fork da atividade 4
+
+	@org.junit.Test
+	public void testTeoresDoSolo() {
+		
+		Solo solo =  new Solo(TexturaSolo.ARGILOSO, 8.59, 0.15, 5.76, 1.63, 3.67, 0.00, 5.35, 30.7);
+		
+		//Assertions dos valores de fosforo, potassio, calcio, magnesio, enxofre e aluminio ideiais
+		assertEquals(9.0, solo.getFosforoIdeal(), 0);
+		assertEquals(0.35, solo.getPotassioIdeal(), 0);
+		assertEquals(6.0, solo.getCalcioIdeal(), 0);
+		assertEquals(1.5, solo.getMagnesioIdeal(), 0);
+		assertEquals(9.0, solo.getEnxofreIdeal(), 0);
+		assertEquals(0.0, solo.getAluminioIdeal(), 0);
+		
+		//Assertions dos campos Scmol, CTCmol, VAtual, TeorMO e Carbono
+		assertEquals(7.54, solo.getSomaBase(), 0);
+		assertEquals(12.89 , solo.getCTCTotal(), 0);
+		assertEquals(58.494957331264544, solo.getSaturacaoPorBases(), 0);
+		assertEquals(3.07, solo.getMOPercentual(), 0);
+		assertEquals(17.848837209302324, solo.getCarbono(), 0);
+	}
+
 }
