43f24f3525820a1e4b09510b3dd0a283f0f64cec
Luiz Henrique Carvalho Silveira <silveira.lh@gmail.com>
2021-05-06 19:19:05 -0300

    Update Atividade4Test para teste de Correção/Recuperação do Potassio
    Assertions dos valores de  quantidade a aplicar, custo  e também dos campos ItemCorreçãoFosforo e ItemCorreçãoFosforo2

--- analise-solo/src/test/java/com/guilhermeeiti/Atividade4Test.java
+++ analise-solo/src/test/java/com/guilhermeeiti/Atividade4Test.java
@@ -1,15 +1,18 @@
 package com.guilhermeeiti;
 
-import static org.junit.Assert.*;
-
 import com.guilhermeeiti.classes.CorrecaoFosforo;
+import com.guilhermeeiti.classes.CorrecaoPotassio;
 import com.guilhermeeiti.classes.Solo;
 import com.guilhermeeiti.enums.FonteFosforo;
+import com.guilhermeeiti.enums.FontePotassio;
 import com.guilhermeeiti.enums.TexturaSolo;
+import junit.framework.TestCase;
 
-public class Atividade4Test {
+public class Atividade4Test extends TestCase {
 	Solo solo =  new Solo(TexturaSolo.ARGILOSO, 8.59, 0.15, 5.76, 1.63, 3.67, 0.00, 5.35, 30.7);
 	CorrecaoFosforo correcaoFosforo = new CorrecaoFosforo(solo, FonteFosforo.SUPERFOSFATO_SIMPLES, 12, 70, 1260.00);
+	CorrecaoPotassio correcaoPotassio = new CorrecaoPotassio(solo, FontePotassio.CLORETO_DE_POTASSIO, 3, 2500.00);
+	
 	@org.junit.Test
 	public void testTeoresDoSolo() {
 		
@@ -44,5 +47,23 @@ public class Atividade4Test {
         assertEquals(34.70622222222223, correcaoFosforo.getItemCorrecaoFosforo2().getValor(), 0);
         assertEquals("Calcio", correcaoFosforo.getItemCorrecaoFosforo2().getNome());
 	}
+	
+	@org.junit.Test
+	public void testCorrRecupPotassio() {
+		
+		//Assertions quantidade a aplicar, custo 
+		assertEquals(450.5462068965517, correcaoPotassio.getQuantidadeAplicar(), 0);
+		assertEquals(1126.3655172413792, correcaoPotassio.getCustoHectar(), 0);
+		
+		//Assertions dos campos ItemCorreçãoPotassio
+		assertEquals(0.0, correcaoPotassio.getItemCorrecaoPotassio().getValor(), 0);
+        assertEquals("", correcaoPotassio.getItemCorrecaoPotassio().getNome());
+        
+        //Assertions dos campos ItemCorreçãoPotassio2
+        assertEquals(0.0, correcaoPotassio.getItemCorrecaoPotassio2().getValor(), 0);
+        assertEquals("", correcaoPotassio.getItemCorrecaoPotassio2().getNome());
+		
+	}
+	
 }
 
